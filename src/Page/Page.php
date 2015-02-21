491122db787c9f537306ffaabcd1460c3f0b9e41
Bert Ramakers <bertramakers@me.com>
2015-02-21 01:16:33 +0100

    Page class now has waitUntilLoaded() method and getBody() method.

--- src/Page/Page.php
+++ src/Page/Page.php
@@ -7,6 +7,8 @@
 
 namespace TwoDotsTwice\Selenium\Page;
 
+use TwoDotsTwice\Selenium\Element\Element;
+
 /**
  * Class Page
  * @package TwoDotsTwice\Selenium\Page
@@ -30,11 +32,24 @@ abstract class Page
     protected $url;
 
     /**
-     * Constructs a new Page object.
+     * Body element on the page.
+     *
+     * @var Element
      */
-    public function __construct(\PHPUnit_Extensions_Selenium2TestCase $testCase)
+    protected $body = null;
+
+    /**
+     * Constructs a new Page object.
+     *
+     * @param \PHPUnit_Extensions_Selenium2TestCase $testCase
+     *   Selenium 2 test case required to interact with the browser.
+     * @param PageUrlInterface $url
+     *   Page url.
+     */
+    public function __construct(\PHPUnit_Extensions_Selenium2TestCase $testCase, PageUrlInterface $url)
     {
         $this->testCase = $testCase;
+        $this->url = $url;
     }
 
     /**
@@ -44,5 +59,45 @@ abstract class Page
     {
         $url = $this->url->getAbsoluteUrl();
         $this->testCase->url($url);
+    }
+
+    /**
+     * Waits until the page is loaded.
+     *
+     * @param int $timeout
+     *   Maximum time to wait for the old body to go stale or the new body to load, in milliseconds.
+     */
+    public function waitUntilLoaded($timeout)
+    {
+        // Check if the body has been set before, which would indicate the page has been loaded before.
+        if (!empty($this->body)) {
+            $body = $this->body;
+
+            // Wait until clicking the old body element throws an exception, which indicates that it's no longer present
+            // on the page.
+            $this->testCase->waitUntil(function () use ($body) {
+                try {
+                    $body->click();
+                    return null;
+                } catch (\PHPUnit_Extensions_Selenium2TestCase_WebDriverException $e) {
+                    return true;
+                }
+            }, $timeout);
+        }
+
+        // Wait until the new body element is present and store it in case the page is reloaded in the future.
+        $criteria = $this->testCase->using('xpath')->value('//body');
+        $this->body = Element::waitUntilFound($this->testCase, $criteria, $timeout);
+    }
+
+    /**
+     * Returns the body element on the page.
+     *
+     * @return Element
+     *   Body element on the page.
+     */
+    public function getBody()
+    {
+        return $this->body;
     }
 }
--- src/Page/PageUrlInterface.php
+++ src/Page/PageUrlInterface.php
@@ -11,9 +11,8 @@ namespace TwoDotsTwice\Selenium\Page;
  * Interface PageUrlInterface
  * @package TwoDotsTwice\Selenium\Page
  *
- * A page url should at least have an absolute url, but may in some cases also
- * contain separate properties of methods for more detailed information about
- * the scheme, domain name, query, ...
+ * A page url should at least have an absolute url, but may in some cases also contain separate properties of methods
+ * for more detailed information about the scheme, domain name, query, ...
  */
 interface PageUrlInterface
 {
